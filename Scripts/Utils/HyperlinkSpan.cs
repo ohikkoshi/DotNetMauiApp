68b85a36f9dcf4c1dfa38324fdaceb4e599ad8cb
ohikkoshi <ohikkoshi@icloud.com>
2024-05-13 15:46:50 +0900

    add hyperlink sample

---
+++ Scripts/Utils/HyperlinkSpan.cs
@@ -0,0 +1,23 @@
+﻿namespace DotNetMauiApp.Utils;
+
+public class HLSpan : Span
+{
+	public static readonly BindableProperty UrlProperty =
+		BindableProperty.Create(nameof(Url), typeof(string), typeof(HLSpan), null);
+
+	public string Url
+	{
+		get { return (string)GetValue(UrlProperty); }
+		set { SetValue(UrlProperty, value); }
+	}
+
+	public HLSpan()
+	{
+		TextDecorations = TextDecorations.Underline;
+		TextColor = Colors.Blue;
+		GestureRecognizers.Add(new TapGestureRecognizer {
+			// Launcher.OpenAsync is provided by Essentials.
+			Command = new Command(async () => await Launcher.OpenAsync(Url))
+		});
+	}
+}
