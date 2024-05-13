5648da78063d92f278f7211584d7a6fb2fdc14ea
ohikkoshi <ohikkoshi@icloud.com>
2024-05-13 17:35:14 +0900

    add theme sample

--- AboutPage.xaml.cs
+++ AboutPage.xaml.cs
@@ -5,5 +5,15 @@ public partial class AboutPage : ContentPage
 	public AboutPage()
 	{
 		InitializeComponent();
+
+		// Default Theme
+		themeSwitch.IsToggled = Application.Current.UserAppTheme == AppTheme.Dark;
+	}
+
+	void OnThemeToggled(object sender, ToggledEventArgs e)
+	{
+		// Perform an action after examining e.Value
+		var theme = e.Value ? AppTheme.Dark : AppTheme.Light;
+		Application.Current.UserAppTheme = theme;
 	}
 }
